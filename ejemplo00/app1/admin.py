70623a616cc00eeacfa547c69d432f331f627dd5
Carlosmicael <carlosmlo381@gmail.com>
2025-06-11 09:16:31 -0500

    segunda

--- ejemplo00/app1/admin.py
+++ ejemplo00/app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Estudiante
+from .models import Ciudad
+
+
+admin.site.register(Estudiante)
+admin.site.register(Ciudad)
--- ejemplo00/app1/models.py
+++ ejemplo00/app1/models.py
@@ -10,3 +10,9 @@ class Estudiante (models.Model):
     def __str__(self):
         return "%s - %s - %s - edad: %d" % (self.nombre,self.apellido,self.cedula,self.edad)
     
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+    poblacion = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} ({self.poblacion} habs)"
